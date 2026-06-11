6c2e63cc3216ca30baca3e6c6b86a71a72e3f8ff
Talha Aslam <ta104g38@gmail.com>
2026-06-12 01:00:41 +0500

    feat: add UserResponse schemas and Nginx configuration file

--- backend/app/schemas/user_schema.py
+++ backend/app/schemas/user_schema.py
@@ -18,8 +18,8 @@ class UserResponse(BaseModel):
     syndicate_coins: int
     battle_pass_tier: int
     battle_pass_xp: int
-    has_premium_pass: bool
-    has_premium_plus: bool
+    has_premium_pass: bool = False
+    has_premium_plus: bool = False
     claimed_free_tiers: List[int]
     claimed_premium_tiers: List[int]
     wins: int
